0733b03df5fadd1ecfb1e0e9823413c5c5f0fdbd
Ramkumar1301investacc <118898628+Ramkumar1301investacc@users.noreply.github.com>
2023-04-03 16:45:40 +0530

    sec

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './component/loginpage/loginpage.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component:LoginpageComponent},
+  {path:'login', component:LoginpageComponent},
+  {path:'navbar', component:NavbarComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
